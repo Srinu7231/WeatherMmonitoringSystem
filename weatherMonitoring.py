7bddf661db95a984644076b25d0e397f4368599a
srinu <103727621+Srinu7231@users.noreply.github.com>
2023-03-25 10:31:49 +0530

    Update weatherMonitoring.py

--- weatherMonitoring.py
+++ weatherMonitoring.py
@@ -1,17 +1,13 @@
 import requests
 
-from twilio.rest import Client
-# account_sid = 'AC708aced84c44441e7e69e61e62ed9c6f'
-# ac = os.environ[account_sid]
-# auth_token = 'c4b990c64c333ecc3e6aaf90f8c5047d'
-# at = os.environ[auth_token]
-account_sid = 'AC708aced84c44441e7e69e61e62ed9c6f'
-auth_token = 'c4b990c64c333ecc3e6aaf90f8c5047d'
+from twilio.rest import Clien
+account_sid = 'your accout SID'
+auth_token = 'Your auth token'
 
 client = Client(account_sid, auth_token)
 
 
-api_key = '1d15d3b3e1eebdf5b0950f5de36c1bb5'
+api_key = 'Your weather api key'
 lat = 18.111240
 lng = 83.397392
 parameters = {
@@ -20,7 +16,7 @@ parameters = {
     "appid": api_key,
     'exclude': 'daily,minutely,current'
 }
-url = 'https://api.openweathermap.org/data/2.5/onecall'
+url = 'https://api.openweathermap.org/data/2.5/onecall'   #goto this link and create your login and create your api key
 response = requests.get(url=url, params=parameters)
 print(response)
 data = response.json()
@@ -36,8 +32,8 @@ for i in twhrs:
         message = client.messages.create(
             body=f"Today it's gonna rain in {hrs}hrs !! Weather Condition:{i['weather'][0]['description']}\n"
                  f" Bring your ☂️ ☂️ ☂️ Umbrilla ☂️ ☂️ ☂️  with you !!",
-            from_='+18317776116',
-            to='+917286874152'
+            from_='TWILIO_NUMBER',
+            to='RECEIVER_NUMBER'
         )
         print(message.status)
         break
